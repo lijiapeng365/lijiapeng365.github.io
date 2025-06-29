b901a3cc3975aea883a7bd5afb897ef2048ab2c8
lijiapeng365 <lijiapeng365@163.com>
2025-06-29 11:32:06 +0800

    Site updated: 2025-06-29 11:32:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751166848384";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751167919534";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
